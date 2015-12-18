5568de027b56218e53fad7bfdf3595a0c0c2cd7b
Dallas <matthews.dallas@gmail.com>
2015-12-17 20:44:24 -0500

    Checkpoint 19 storyboarding: complete

---
+++ Proteinolator/Proteinolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Proteinolator
+//
+//  Created by Dallas S Matthews on 12/17/15.
+//  Copyright (c) 2015 Dallas S Matthews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
